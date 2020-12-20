701e54d1412ccf60d935991aa3b1769dbef40b20
Александр Плохой <plohoy@zen-it.ru>
2020-12-20 10:46:42 +0300

    add packages

--- src/main/java/com/plohoy/generator/GeneratorApplication.java
+++ src/main/java/com/plohoy/generator/GeneratorApplication.java
@@ -2,6 +2,7 @@ package com.plohoy.generator;
 
 public class GeneratorApplication {
     public static void main(String[] args) {
-        
+        StringBuilder string = new StringBuilder();
+
     }
 }
